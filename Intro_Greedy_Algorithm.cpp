4c951852bce0b4a44869532e576b44a2e3c6ce28
TseWangPok_Jerry <126223772+JerryTseee@users.noreply.github.com>
2024-06-16 15:37:43 +0800

    Add files via upload

---
+++ Intro_Greedy_Algorithm.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+/*
+Greedy algorithm does not produce an optimal solution, but can yield locally optimal
+solutions that approxiamte a globally optimal solution in a reasonable amount of time
+
+The choice made by a greedy algorithm may depend on choices made so far,
+but not on future choices or all the solutions to the subproblem.
+It iteratively makes one greedy choice after another. (Not like dynamic programming
+which makes decisions based on all the decisions made in previous stage)
+*/
+
+
+//Example: Dijkstra's Algorithm: Find the shortest path from a source vertex to all other vertices in a weighted graph
+
+
+int main(){
+
+    return 0;
+}
